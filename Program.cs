07c3ca7467ee450f63d0bb0eb55bbc9407181325
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-23 11:36:35 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Leia um número inteiro e exiba "Negativo", "Zero" ou "Positivo", conforme o caso.
+
+Console.WriteLine("--- lendo número ---\n");
+
+Console.Write("Digite um número inteiro: ");
+int numint = int.Parse(Console.ReadLine()!);
+
+if (numint < 0) {
+    Console.WriteLine("\nNegativo");
+}else if (numint == 0) {
+    Console.WriteLine("\nero");
+}else {
+    Console.WriteLine("\nPositivo");
+}
+
+
+
